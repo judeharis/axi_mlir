a4aa1a6ff92be62016afaf00e514bc9ba695cd46
Nicolas Agostini <n.b.agostini@gmail.com>
2022-04-16 08:40:29 -0700

    Implement compilation example for CPU tiling and update tracked llvm-project

--- cross-comp/accell/compile.sh
+++ cross-comp/accell/compile.sh
@@ -7,13 +7,39 @@ set -x
 # target datalayout = "e-m:e-p:32:32-i64:64-v128:64:128-a:0:32-n32-S64"
 # target triple = "armv7-none-linux-android"
 
+OUTDIR=output
 export PROJ_ROOT=/working_dir
+mkdir -p $OUTDIR
 
 BITW=64
 
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-opt \
+#         -test-linalg-to-axi4mlir="flow-cpu-accumulation" \
+#         -convert-linalg-to-loops -lower-affine -convert-scf-to-cf   \
+#         -arith-expand \
+#         -memref-expand \
+#         -convert-vector-to-llvm \
+#         -convert-memref-to-llvm="index-bitwidth=$BITW" \
+#         -convert-arith-to-llvm="index-bitwidth=$BITW" \
+#         -convert-std-to-llvm="index-bitwidth=$BITW" \
+#         -reconcile-unrealized-casts \
+#         $PROJ_ROOT/llvm-project/mlir/test/axi4mlir-runner/run-matmul-v1accel.mlir \
+#         -o llvm.mlir \
+#         -print-ir-before-all 2>&1 | cat > intermediate.mlir
+
+# Declare an array of string with type
+declare -a StringArray=(
+    "run-matmul-v1accel-tile_acc-small-m60_n72_k80"
+    "run-matmul-v1accel-tile_l2-small-m60_n72_k80"
+)
+ 
+# Iterate the string array using for loop
+for INPUT in ${StringArray[@]}; do
+
 $PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-opt \
-        -test-linalg-to-axi4mlir="flow-cpu-accumulation" \
-        -convert-linalg-to-loops -convert-scf-to-cf   \
+        -test-linalg-to-axi4mlir="flow-cpu-accumulation tile-sizes=128,128,128,32,32,32" \
+        -convert-linalg-to-loops -lower-affine --buffer-loop-hoisting --buffer-deallocation \
+        -convert-scf-to-cf  \
         -arith-expand \
         -memref-expand \
         -convert-vector-to-llvm \
@@ -21,21 +47,24 @@ $PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-opt \
         -convert-arith-to-llvm="index-bitwidth=$BITW" \
         -convert-std-to-llvm="index-bitwidth=$BITW" \
         -reconcile-unrealized-casts \
-        $PROJ_ROOT/llvm-project/mlir/test/axi4mlir-runner/run-matmul-v1accel.mlir \
-        -o llvm.mlir \
-        -print-ir-before-all 2>&1 | cat > intermediate.mlir
+        sources/$INPUT.mlir \
+        -o $OUTDIR/llvm.mlir 
+        # \
+        # -print-ir-before-all 2>&1 | cat > intermediate.mlir
 
-$PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-translate --mlir-to-llvmir llvm.mlir -o app.ll
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-translate --mlir-to-llvmir $OUTDIR/llvm.mlir -o $OUTDIR/app.ll
 
 $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -g \
     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
-    -c -o app.o app.ll
+    -c -o $OUTDIR/app.o $OUTDIR/app.ll
 
-$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -g -o app app.o \
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -g -o $OUTDIR/$INPUT-app $OUTDIR/app.o \
     --target=arm-linux-gnueabihf \
     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
     -lmlir_runner_utils -lmlir_axi_runner_utils
+   
+done
 
 set +e
 set +x
--- cross-comp/accell/run.sh
+++ cross-comp/accell/run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-source compile.sh
+if [[ $1 == "" ]]; then
+ echo "Error: You did not provide an argument."
+ echo "  Usage:         $0 <path-to-arm-binary>"
+ echo "  Usage example: $0 output/run-matmul-v1accel-mini-app"
+ exit 0
+
+fi
+
+# Uncomment to pre-compile
+# source compile.sh
 
 # Running with qemu
-qemu-arm -L /usr/arm-linux-gnueabihf ./app
+qemu-arm -L /usr/arm-linux-gnueabihf $1
