a3c3237057a9466ab2f63bdc5b6c1af025b41f89
Nicolas Agostini <n.b.agostini@gmail.com>
2022-04-23 00:02:51 -0700

    WIP: C driver compilation implemented, but segfaults during execution

--- cross-comp/accell/generated/matmul_driver.c
+++ cross-comp/accell/generated/matmul_driver.c
@@ -20,7 +20,7 @@
 // https://mlir.llvm.org/docs/TargetLLVMIR/#calling-conventions for details.
 //
 // The function takes 3 2D memref, the signature in MLIR LLVM dialect will be:
-// llvm.func @matmul_m4_n4_k4_L1_call(
+// llvm.func @matmul_m8_n8_k8_L1_call(
 //   // First Memref (%arg0)
 //      %allocated_ptr0: !llvm.ptr<int>, %aligned_ptr0: !llvm.ptr<int>,
 //      %offset0: i64, %size0_d0: i64, %size0_d1: i64, %stride0_d0: i64,
@@ -34,7 +34,7 @@
 //      %offset2: i64, %size2_d0: i64, %size2_d1: i64, %stride2_d0: i64,
 //      %stride2_d1: i64,
 //
-void matmul_m4_n4_k4_L1_call(
+void matmul_m8_n8_k8_L1_call(
     int *allocated_ptr0, int *aligned_ptr0,
     intptr_t offset0, intptr_t size0_d0, intptr_t size0_d1,
     intptr_t stride0_d0, intptr_t stride0_d1,
@@ -47,7 +47,7 @@ void matmul_m4_n4_k4_L1_call(
     intptr_t stride2_d0, intptr_t stride2_d1);
 
 // The llvm.emit_c_interface will also trigger emission of another wrapper:
-// llvm.func @_mlir_ciface_matmul_m4_n4_k4_L1_call(
+// llvm.func @_mlir_ciface_matmul_m8_n8_k8_L1_call(
 //   %arg0: !llvm.ptr<struct<(ptr<i32>, ptr<i32>, i64,
 //                            array<2 x i64>, array<2 x i64>)>>,
 //   %arg1: !llvm.ptr<struct<(ptr<i32>, ptr<i32>, i64,
@@ -63,40 +63,28 @@ typedef struct
   intptr_t size[2];
   intptr_t stride[2];
 } memref_2d_descriptor;
-long long _mlir_ciface_matmul_m4_n4_k4_L1_call(
+void _mlir_ciface_matmul_m8_n8_k8_L1_call(
     memref_2d_descriptor *arg0,
     memref_2d_descriptor *arg1,
     memref_2d_descriptor *arg2);
 
-#define M 4
-#define N 4
-#define K 4
+#define M 8
+#define N 8
+#define K 8
 int arg0[M][K];
 int arg1[K][N];
 int arg2[M][N];
 
-void dump()
-{
-  for (int i = 0; i < M; i++)
-  {
+void dump() {
+  for (int i = 0; i < N; i++) {
     printf("[");
-    for (int j = 0; j < K; j++)
+    for (int j = 0; j < M; j++)
       printf("%d,\t", (int)arg0[i][j]);
     printf("] [");
-  }
-
-  for (int i = 0; i < K; i++)
-  {
-    printf("[");
-    for (int j = 0; j < N; j++)
+    for (int j = 0; j < M; j++)
       printf("%d,\t", (int)arg1[i][j]);
-    printf("]\n");
-  }
-
-  for (int i = 0; i < M; i++)
-  {
-    printf("[");
-    for (int j = 0; j < N; j++)
+    printf("] [");
+    for (int j = 0; j < M; j++)
       printf("%d,\t", (int)arg2[i][j]);
     printf("]\n");
   }
@@ -119,7 +107,8 @@ int main()
   dump();
 
   // Call into MLIR.
-  matmul_m4_n4_k4_L1_call(
+  printf("Call into MLIR\n");
+  matmul_m8_n8_k8_L1_call(
       (int *)arg0, (int *)arg0, 0, M, K, K, 0,
       //
       (int *)arg1, (int *)arg1, 0, K, N, N, 0,
@@ -151,7 +140,8 @@ int main()
   memref_2d_descriptor arg2_descriptor = {
       (int *)arg2, (int *)arg2, 0, M, N, N, 0};
 
-  _mlir_ciface_matmul_m4_n4_k4_L1_call(&arg0_descriptor, &arg1_descriptor, &arg2_descriptor);
+  printf("Call into MLIR with ciface\n");
+  _mlir_ciface_matmul_m8_n8_k8_L1_call(&arg0_descriptor, &arg1_descriptor, &arg2_descriptor);
 
   printf("Result: C[0,0]=%d\n", arg2[0][0]);
 
