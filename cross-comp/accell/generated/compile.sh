2b39381e2f0fbfa8276b91e8ff2ad61df186b884
judeharis <2191920h@student.gla.ac.uk>
2022-04-29 19:22:01 +0100

    Integrated C++ tiling call into Matmul driver

--- cross-comp/accell/generated/compile.sh
+++ cross-comp/accell/generated/compile.sh
@@ -11,6 +11,7 @@ OUTDIR=output
 export PROJ_ROOT=/working_dir
 mkdir -p $OUTDIR
 
+APICC=$PROJ_ROOT/llvm-project/mlir/lib/ExecutionEngine/axi
 BITW=64
 
 # Declare an array of string with type
@@ -40,29 +41,111 @@ $PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-opt \
     # \
     # -print-ir-before-all 2>&1 | cat > intermediate.mlir
 
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-translate --mlir-to-llvmir $OUTDIR/llvm.mlir -o $OUTDIR/libmlirmatmuls.ll
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang \
+#     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
+#     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
+#     -c -o $OUTDIR/libmlirmatmuls.o $OUTDIR/libmlirmatmuls.ll
+
+# ar -rv $OUTDIR/libmlirmatmuls.a $OUTDIR/libmlirmatmuls.o
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -shared -o $OUTDIR/libmlirmatmuls.so $OUTDIR/libmlirmatmuls.o \
+#     --target=arm-linux-gnueabihf \
+#     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -lmlir_runner_utils -lmlir_axi_runner_utils
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -o $OUTDIR/matmuldriver-app \
+#     $OUTDIR/libmlirmatmuls.o srcs/matmul_driver.c \
+#     -Isrcs \
+#     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
+#     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
+#     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -lmlir_runner_utils -lmlir_axi_runner_utils
+
+
 $PROJ_ROOT/builds/llvm-project/build-x86/bin/mlir-translate --mlir-to-llvmir $OUTDIR/llvm.mlir -o $OUTDIR/libmlirmatmuls.ll
 
-$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang \
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ \
     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
     -c -o $OUTDIR/libmlirmatmuls.o $OUTDIR/libmlirmatmuls.ll
 
 ar -rv $OUTDIR/libmlirmatmuls.a $OUTDIR/libmlirmatmuls.o
 
-$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -shared -o $OUTDIR/libmlirmatmuls.so $OUTDIR/libmlirmatmuls.o \
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -shared -o $OUTDIR/libmlirmatmuls.so $OUTDIR/libmlirmatmuls.o \
     --target=arm-linux-gnueabihf \
     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
     -lmlir_runner_utils -lmlir_axi_runner_utils
 
-$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -o $OUTDIR/matmuldriver-app \
-    $OUTDIR/libmlirmatmuls.o srcs/matmul_driver.c \
+# Creates the standalone AXI libthe
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -c -o $OUTDIR/pynq_api_v1.o $APICC/api_v1.cpp \
+    --target=arm-linux-gnueabihf \
+    -fPIC -DREAL -I$PROJ_ROOT/llvm-project/mlir/include
+
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -shared -o $OUTDIR/libpynq_api_v1.so $OUTDIR/pynq_api_v1.o \
+    --target=arm-linux-gnueabihf \
+    -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+    -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib
+
+# Use this to include the standalone AXI lib for C++ drivers
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -o $OUTDIR/matmuldriver-app \
+    $OUTDIR/libmlirmatmuls.o srcs/matmul_driver_v2.cc \
     -Isrcs \
+    -I$PROJ_ROOT/llvm-project/mlir/include \
     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
-    -lmlir_runner_utils -lmlir_axi_runner_utils
+    -L$OUTDIR/ \
+    -lmlir_runner_utils -lmlir_axi_runner_utils -lpynq_api_v1
+
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -o $OUTDIR/matmuldriver-app \
+#     $OUTDIR/libmlirmatmuls.o srcs/matmul_driver_v2.cc \
+#     -Isrcs \
+#     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
+#     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
+#     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -Wl,-rpath=$OUTDIR \
+#     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -L$OUTDIR \
+#     -lmlir_runner_utils -lmlir_axi_runner_utils -lmlirmatmuls
+
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -v -x c -E \
+#     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
+#     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
+#     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib:`pwd`/$OUTDIR \
+#     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -L$OUTDIR \
+#     -lmlir_runner_utils -lmlir_axi_runner_utils -lmlirmatmuls \
+#     - </dev/null
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang -v -x c -E \
+#     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
+#     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
+#     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib:`pwd`/$OUTDIR \
+#     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -L$OUTDIR \
+#     -lmlir_runner_utils -lmlir_axi_runner_utils -lmlirmatmuls \
+#     - </dev/null
+
+# $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++  -o $OUTDIR/matmuldriver-app \
+#     srcs/matmul_driver_v2.cc \
+#     -std=c++11 \
+#     -stdlib=libstdc++ \
+#     -Isrcs \
+#     --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
+#     -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
+#     -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib:`pwd`/$OUTDIR \
+#     -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+#     -L$OUTDIR \
+#     -lmlir_runner_utils -lmlir_axi_runner_utils -lmlirmatmuls
    
 done
 
---
+++ cross-comp/accell/generated/srcs/mm4x4v1_ts1.h
@@ -0,0 +1,68 @@
+#include "mlir/ExecutionEngine/axi/api_v1.h"
+
+#define tile_M 4
+#define tile_N 4
+#define tile_K 4
+#define M 8
+#define N 8
+#define K 8
+
+void v1_ts1(int A[M][K], int B[K][N], int C[M][N]) {
+  //   LOG("=========================");
+  //   LOG("ACC: MM_4x4v1");
+  //   LOG("Tiling Strat: 1");
+  //   LOG("=========================");
+
+  // Init DMA + ACC
+  struct dma dma1;
+  dma1.dma_init(0x40400000, 0x16000000, 65536, 0x16400000, 65536);
+
+  // Start Tiling
+  for (int k = 0; k < K; k += tile_K) {
+    for (int n = 0; n < N; n += tile_N) {
+      for (int m = 0; m < M; m += tile_M) {
+
+        // Gets pointer to DMA_IN_BUFFER
+        unsigned int *dma_inbuffer = dma1.dma_get_inbuffer();
+
+        // Data_len is used to track what is in the DMA_IN_BUFFER
+        int data_len = 0;
+
+        // Copies A into DMA_IN_BUFFER; Increments data_len by length of A
+
+        for (int tm = 0; tm < tile_M; tm++)
+          for (int tk = 0; tk < tile_K; tk++)
+            dma_inbuffer[data_len + tile_K * tm + tk] = A[m + tm][k + tk];
+        data_len += tile_M * tile_K;
+
+        for (int tn = 0; tn < tile_N; tn++)
+          for (int tk = 0; tk < tile_K; tk++)
+            dma_inbuffer[data_len + tile_K * tn + tk] = B[k + tk][n + tn];
+        data_len += tile_N * tile_K;
+
+        // Sends data_len of data
+        dma1.dma_start_send(data_len, 0);
+
+        // Waits for data to transfer to finish
+        dma1.dma_wait_send();
+
+        // Indicates to DMA, how much space is available and where it is
+        dma1.dma_start_recv(tile_N * tile_M, 0);
+
+        // Waits for data to be received (including TLAST signal)
+        dma1.dma_wait_recv();
+
+        // Gets pointer to DMA_OUT_BUFFER
+        unsigned int *dma_outbuffer = dma1.dma_get_outbuffer();
+
+        // Copies result from DMA_OUT_BUFFER to padded output buffer
+        for (int tn = 0; tn < tile_N; tn++) {
+          for (int tm = 0; tm < tile_M; tm++) {
+            C[m + tm][n + tn] += dma_outbuffer[tile_M * tn + tm];
+          }
+        }
+      }
+    }
+  }
+  dma1.dma_free();
+}
