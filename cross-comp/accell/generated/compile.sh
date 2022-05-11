c564611eb7fa121844a2317cf275fa92bdc79014
Nicolas Agostini <n.b.agostini@gmail.com>
2022-05-11 10:45:34 -0700

    Include all possible parameters to compile options

--- cross-comp/accell/generated/compile.sh
+++ cross-comp/accell/generated/compile.sh
@@ -15,32 +15,46 @@ APICC=$PROJ_ROOT/llvm-project/mlir/lib/ExecutionEngine/axi
 BITW=64
 
 
-# CONFIGS
+# Static CONFIGS
 KERNEL_NAME=matmul
 ACCEL_SIZE=4
 STRATEGY=MEM
-SHARED_DIM=64
+PROBLEM_DIM=64
 
-M=$SHARED_DIM
-N=$SHARED_DIM
-K=$SHARED_DIM
-DIMS=m${M}_n${N}_k${K}
-CALL_NAME=${KERNEL_NAME}_${DIMS}_${STRATEGY}
-MLIR_CALL=${CALL_NAME}_call
-MLIRMATMULCALL=$MLIR_CALL
-CIMLIRMATMULCALL=_mlir_ciface_$MLIR_CALL
 
-MLIR_CPU_CALL=${CALL_NAME}_call
-MLIRMATMULCALLCPU=${MLIR_CPU_CALL}
-CIMLIRMATMULCALLCPU=_mlir_ciface_${MLIR_CPU_CALL}
+# ===========================
+# Declaring input arrays
 
-RUN_NAME=${KERNEL_NAME}-${DIMS}-${STRATEGY}-acc$ACCEL_SIZE
-APPNAME=driver-${RUN_NAME}-app
-
-# Declare an array of string with type
+# Used by both MLIR MATMUL library and final app
 declare -a AccelSizeArray=(
     "4"
 )
+
+declare -a KernelNameArray=(
+    "matmul"
+)
+
+declare -a StrategyArray=(
+    "MEM"
+    "L2"
+    "L1"
+    "CPU"
+    "MANUAL"
+)
+
+declare -a ProblemDimArray=(
+    "4"
+    "8"
+    "16"
+    "32"
+    "64"
+    "128"
+    "256"
+    "512"
+)
+
+# ===========================
+# Compiling mlir matmul library for a given accelerator size
 
 # Iterate the string array using for loop
 for ACCEL_SIZE in ${AccelSizeArray[@]}; do
@@ -84,45 +98,73 @@ $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -shared -o $OUTDIR/libmlirm
     -lmlir_runner_utils -lmlir_axi_runner_utils
 done
 
-# Declare an array of string with type
-declare -a StringArray=(
-    "mlir_matmuls"
-)
+# ===========================
+# Compiling output binary for a given problem, strategy, accel size
 
 # Iterate the string array using for loop
-for INPUT in ${StringArray[@]}; do
+for STRATEGY in ${StrategyArray[@]}; do
+for KERNEL_NAME in ${KernelNameArray[@]}; do
+for PROBLEM_DIM in ${ProblemDimArray[@]}; do
+for ACCEL_SIZE in ${AccelSizeArray[@]}; do
 
-    #TODO if MANUAL Strategy, make empty
-    #TODO if
+M=$PROBLEM_DIM
+N=$PROBLEM_DIM
+K=$PROBLEM_DIM
+DIMS=m${M}_n${N}_k${K}
+CALL_NAME=${KERNEL_NAME}_${DIMS}_${STRATEGY}
+MLIR_CALL=${CALL_NAME}_call
+MLIRMATMULCALL=$MLIR_CALL
+CIMLIRMATMULCALL=_mlir_ciface_$MLIR_CALL
+
+MLIR_CPU_CALL=${CALL_NAME}_call
+MLIRMATMULCALLCPU=${MLIR_CPU_CALL}
+CIMLIRMATMULCALLCPU=_mlir_ciface_${MLIR_CPU_CALL}
+
+RUN_NAME=${KERNEL_NAME}-${DIMS}-${STRATEGY}-acc$ACCEL_SIZE
+APPNAME=driver-${RUN_NAME}-app
+
+if [ "$STRATEGY" == "MANUAL" ]; then
+    # Compiling driver implemented by hand
+    ADDITIONAL_FLAGS=-DRUNCPP
+elif [ "$STRATEGY" == "CPU" ]; then
+    # Compiling driver in MLIR without acceleration
+    ADDITIONAL_FLAGS=""
+    RUN_NAME=${KERNEL_NAME}-${DIMS}-${STRATEGY}-accNONE
+    APPNAME=driver-${RUN_NAME}-app
+else
+    # Compiling driver implemented in MLIR
     ADDITIONAL_FLAGS=-DRUNMLIR
+fi
 
-    # Use this to include the standalone AXI lib for C++ drivers
-    $PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -o $OUTDIR/$APPNAME \
-        srcs/matmul_driver_v3.cc \
-        -Isrcs \
-        -I$PROJ_ROOT/llvm-project/mlir/include \
-        --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
-        -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
-        -Wl,--copy-dt-needed-entries \
-        -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
-        -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
-        -lmlir_runner_utils -lmlir_c_runner_utils -lmlir_axi_runner_utils \
-        -L$OUTDIR \
-        -lmlirmatmuls_acc${ACCEL_SIZE} \
-        -Dtile_M=$ACCEL_SIZE \
-        -Dtile_N=$ACCEL_SIZE \
-        -Dtile_K=$ACCEL_SIZE \
-        -DM=$M \
-        -DN=$N \
-        -DK=$K \
-        -DMLIRMATMULCALL=$MLIRMATMULCALL \
-        -DCIMLIRMATMULCALL=$CIMLIRMATMULCALL \
-        -DMLIRMATMULCALLCPU=$MLIRMATMULCALLCPU \
-        -DCIMLIRMATMULCALLCPU=$CIMLIRMATMULCALLCPU \
-        -DRUNMLIR
+# Use this to include the standalone AXI lib for C++ drivers
+$PROJ_ROOT/builds/llvm-project/build-x86/bin/clang++ -o $OUTDIR/$APPNAME \
+    srcs/matmul_driver_v3.cc \
+    -Isrcs \
+    -I$PROJ_ROOT/llvm-project/mlir/include \
+    --target=arm-linux-gnueabihf -march=armv7-a -marm -mfloat-abi=hard \
+    -mfpu=neon -funsafe-math-optimizations -ftree-vectorize \
+    -Wl,--copy-dt-needed-entries \
+    -Wl,-rpath=$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+    -L$PROJ_ROOT/builds/llvm-project/build-runner-arm/lib \
+    -lmlir_runner_utils -lmlir_c_runner_utils -lmlir_axi_runner_utils \
+    -L$OUTDIR \
+    -lmlirmatmuls_acc${ACCEL_SIZE} \
+    -Dtile_M=$ACCEL_SIZE \
+    -Dtile_N=$ACCEL_SIZE \
+    -Dtile_K=$ACCEL_SIZE \
+    -DM=$M \
+    -DN=$N \
+    -DK=$K \
+    -DMLIRMATMULCALL=$MLIRMATMULCALL \
+    -DCIMLIRMATMULCALL=$CIMLIRMATMULCALL \
+    -DMLIRMATMULCALLCPU=$MLIRMATMULCALLCPU \
+    -DCIMLIRMATMULCALLCPU=$CIMLIRMATMULCALLCPU \
+    $ADDITIONAL_FLAGS
 
-        # -DRUNCPP \
-done
+done #AccelSizeArray
+done #ProblemDimArray
+done #KernelNameArray
+done #StrategyArray
 
 set +e
 set +x
