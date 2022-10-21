f96a17e7575f2622421c8002660c7ff0cd924350
judeharis <2191920h@student.gla.ac.uk>
2022-10-21 13:14:39 +0100

    Added ConvV2 accelerator code

---
+++ api_tester/accelerators/convV2/accelerator.sc.cc
@@ -0,0 +1,136 @@
+#include "accelerator.sc.h"
+
+
+void ACCNAME::Recv() {
+	ic.write(0);
+	read_si.write(1);
+  wait();
+  unsigned int start_count=0;
+  while (1) {
+		read_si.write(4);
+		opcode packet(din1.read().data);
+
+		if (packet.set_channels) {
+			int nic = din1.read().data;
+			ic.write(nic);
+			read_co.write(ic);
+		}
+
+
+    if (packet.read_inputs) {
+			for (int c = 0; c < ic; c++) {
+#pragma HLS pipeline
+				for (int h = 0; h < H; h++) {
+					for (int w = 0; w < W; w++) {
+						inputs[h][w][c] = din1.read().data;
+					}
+				}
+			}
+    }
+
+		read_si.write(5);
+		if (packet.read_fliters) {
+			for (int c = 0; c < ic; c++) {
+#pragma HLS pipeline
+				for (int h = 0; h < H; h++) {
+					for (int w = 0; w < W; w++) {
+						filters[h][w][c] = din1.read().data;
+					}
+				}
+			}
+		}
+
+		read_si.write(6);
+    if (packet.compute_outputs) {
+      compute.write(true);
+      wait();
+    }
+    wait();
+		while (compute)
+			wait();
+
+    if (packet.send_outputs) {
+      wait();
+      send.write(true);
+    }
+
+    while (send)
+      wait();
+
+  }
+}
+
+void ACCNAME::Compute() {
+	int ic_acc[4];
+	compute_si.write(0);
+  wait();
+  while (1) {
+  	compute_si.write(1);
+    while (!compute)
+      wait();
+
+    compute_si.write(2);
+    wait();
+
+    for (int c = 0; c < 4; c++) ic_acc[c]=0;
+
+    for (int c = 0; c < ic; c++) {
+#pragma HLS unroll factor =4
+#pragma HLS pipeline
+			for (int h = 0; h < H; h++) {
+				for (int w = 0; w < W; w++) {
+					int x = inputs[h][w][c];
+					int y = filters[h][w][c];
+					ic_acc[c%4] += mul_int32(x,y);
+				}
+			}
+		}
+		output[0]+=ic_acc[0]+ic_acc[1]+ic_acc[2]+ic_acc[3];
+		read_so.write(ic_acc[0]);
+    compute_si.write(3);
+    wait();
+    compute.write(false);
+    wait();
+  }
+}
+
+int ACCNAME::mul_int32(int x, int y) {
+	return x * y;
+}
+
+void ACCNAME::Send() {
+	send_si.write(0);
+  wait();
+  while (1) {
+  	send_si.write(1);
+    while (!send)
+      wait();
+    send_si.write(2);
+
+		DATA d;
+		d.tlast = true;
+		d.data = output[0];
+		dout1.write(d);
+		wait();
+		output[0]=0;
+
+    send_si.write(3);
+    send.write(false);
+    wait();
+  }
+}
+
+//void ACCNAME::all_counters(){
+//	wait();
+//	while(true){
+//		{
+//#pragma HLS LATENCY max=0 min=0
+//#pragma HLS protocol fixed
+//			HWC_Func(read)
+//			HWC_Func(compute)
+//			HWC_Func(send)
+//		}
+//	}
+//}
+
+
---
+++ api_tester/accelerators/convV2/accelerator.sc.h
@@ -0,0 +1,162 @@
+#ifndef CONV2D_H
+#define CONV2D_H
+
+#include <systemc.h>
+
+#ifndef __SYNTHESIS__
+#define DWAIT(x) wait(x)
+#else
+#define DWAIT(x)
+#endif
+
+#define H 3 // Filter H
+#define W 3 // Filter W
+#define C 128 // Input C
+
+#define PRAGMA(X) _Pragma(#X)
+
+#define HWC_AXI4LiteS(name,signame) PRAGMA(HLS resource core=AXI4LiteS metadata="-bus_bundle hwc" variable=name##_##signame)
+
+#define HWC_PragGroup(name) HWC_AXI4LiteS(name,sts) \
+	HWC_AXI4LiteS(name,co) \
+	HWC_AXI4LiteS(name,so)
+
+#define HWC_Define(name) 	sc_in<unsigned int> name##_sts; \
+	sc_signal<unsigned int> name##_si; \
+	sc_out<unsigned int> name##_co; \
+	sc_out<unsigned int> name## _so; \
+	unsigned int name##_cycles;
+
+typedef struct _DATA {
+  sc_uint<32> data;
+  bool tlast;
+  inline friend ostream &operator<<(ostream &os, const _DATA &v) {
+    cout << "data&colon; " << v.data << " tlast: " << v.tlast;
+    return os;
+  }
+} DATA;
+
+#define ACCNAME CONV_3x3
+#ifdef VERBOSE_ACC
+#define ALOG(x) std::cout << x << std::endl
+#else
+#define ALOG(x)
+#endif
+
+// OP-Code Stuct
+// 0000 : 0 = NOP;
+// 0001 : 1 = read_fliters;
+// 0010 : 2 = read_inputs;
+// 0011 : 3 = read_fliters -> read_inputs;
+// 0100 : 4 = compute_outputs;
+// 0101 : 5 = read_fliters -> compute_outputs;
+// 0110 : 6 = read_inputs -> compute_outputs;
+// 0111 : 7 = read_fliters -> read_inputs -> compute_outputs;
+
+// 1000 : 8 = send_outputs;
+// 1001 : 9 = read_fliters -> send_outputs;
+// 1010 : 10 = read_inputs -> send_outputs;
+// 1011 : 11 = read_fliters -> read_inputs -> send_outputs;
+// 1100 : 12 = compute_outputs -> send_outputs;
+// 1101 : 13 = read_fliters -> compute_outputs -> send_outputs;
+// 1110 : 14 = read_inputs -> compute_outputs -> send_outputs;
+// 1111 : 15 = read_fliters -> read_inputs -> compute_outputs -> send_outputs;
+
+// 10000 : 16 = set_channels;
+
+struct opcode {
+  unsigned int packet;
+  bool read_fliters;
+  bool read_inputs;
+  bool compute_outputs;
+  bool send_outputs;
+  bool set_channels;
+
+  opcode(sc_uint<32> _packet) {
+    ALOG("OPCODE: " << _packet);
+    ALOG("Time: " << sc_time_stamp());
+    packet = _packet;
+    read_fliters = _packet.range(0, 0);
+    read_inputs = _packet.range(1, 1);
+    compute_outputs = _packet.range(2, 2);
+    send_outputs = _packet.range(3, 3);
+    set_channels = _packet.range(4, 4);
+  }
+};
+
+
+
+SC_MODULE(ACCNAME) {
+  sc_in<bool> clock;
+  sc_in<bool> reset;
+
+
+  sc_int<32> inputs[H][W][C];
+  sc_int<32> filters[H][W][C];
+  sc_int<32> output[16];
+  sc_fifo_in<DATA> din1;
+  sc_fifo_out<DATA> dout1;
+
+
+
+
+#ifndef __SYNTHESIS__
+  sc_signal<bool, SC_MANY_WRITERS> compute;
+  sc_signal<bool, SC_MANY_WRITERS> send;
+#else
+  sc_signal<bool> compute;
+  sc_signal<bool> send;
+#endif
+
+	HWC_Define(read);
+	HWC_Define(compute);
+	HWC_Define(send);
+
+	sc_signal<int> ic;
+
+  void Recv();
+
+  void Compute();
+
+  void Send();
+
+  int mul_int32(int,int);
+
+
+  void print_profile();
+
+  SC_HAS_PROCESS(ACCNAME);
+
+  ACCNAME(sc_module_name name_) : sc_module(name_) // @suppress("Class members should be properly initialized")
+  {
+    SC_CTHREAD(Recv, clock.pos());
+    reset_signal_is(reset, true);
+
+    SC_CTHREAD(Compute, clock.pos());
+    reset_signal_is(reset, true);
+
+    SC_CTHREAD(Send, clock.pos());
+    reset_signal_is(reset, true);
+
+
+	#pragma HLS RESOURCE variable=din1 core=AXI4Stream metadata="-bus_bundle S_AXIS_DATA1" port_map={{din1_0 TDATA} {din1_1 TLAST}}
+	#pragma HLS RESOURCE variable=dout1 core=AXI4Stream metadata="-bus_bundle M_AXIS_DATA1" port_map={{dout1_0 TDATA} {dout1_1 TLAST}}
+	#pragma HLS RESET variable=reset
+
+#pragma HLS array_partition variable=inputs cyclic dim=3 factor=4
+#pragma HLS array_partition variable=filters cyclic dim=3 factor=4
+
+#pragma HLS array_partition variable=inputs complete dim=1
+#pragma HLS array_partition variable=inputs complete dim=2
+
+#pragma HLS array_partition variable=filters complete dim=1
+#pragma HLS array_partition variable=filters complete dim=2
+
+		HWC_PragGroup(read)
+		HWC_PragGroup(compute)
+		HWC_PragGroup(send)
+  }
+};
+
+
+#endif
